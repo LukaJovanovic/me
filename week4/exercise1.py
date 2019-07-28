de8f9e546690552b7aa8ffc4f74abf97951b623a
LukaJovanovic <llukajovanovicc@gmail.com>
2019-07-28 18:28:44 +1000

    the wordy pyramid now works

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -88,19 +88,19 @@ def wordy_pyramid():
     pyraList = []
     number = 3
     limit = 20
-    url = "http://api.wordnik.com/v4/words.json/randomWords?api_key=zau1khp6480m0ik9quh0pkcel471yx59gmv2wtnznfgn41nxn&minLength={}".format(number) + "&maxLength={}&limit=1".format(number)
+    url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={}".format(number)
     #While it is going up
     while True:
         if number <= limit:
-            url = "http://api.wordnik.com/v4/words.json/randomWords?api_key=zau1khp6480m0ik9quh0pkcel471yx59gmv2wtnznfgn41nxn&minLength={}".format(number) + "&maxLength={}&limit=1".format(number)
+            url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={}".format(number)
             r = requests.get(url)
-            response_json = json.loads(r.text)
+            response_json = r.text
             #while loop to keep asking until there is no error
             #can get constantly stuck in error 429
             while r.status_code != 200:
                 r = requests.get(url)
                 response_json = json.loads(r.text)
-            pyraList.append(response_json[0]["word"])
+            pyraList.append(response_json)
             number = number + 2
             #while going up by 2
             print(pyraList)
--- week4/readme.md
+++ week4/readme.md
@@ -2,3 +2,4 @@
 remeber to re-reference and append the_json like in pokemon for future reference. 
 if permission isnt granted to access a file in python then provide more of the directory 
 e.g week4/lasers.pew -->
+#only use json.loads with the r.text when it is a json file, and not text. be careful with that, obviously.
